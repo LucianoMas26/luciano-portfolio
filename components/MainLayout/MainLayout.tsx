d9c5ee2e66ab63c3ef0323423b4700b11c85cfb5
Luciano <lucianoalejandromas@gmail.com>
2023-08-19 18:10:10 -0300

    "Adding syles and components"

---
+++ components/MainLayout/MainLayout.tsx
@@ -0,0 +1,15 @@
+export const MainLayout = ({
+  children,
+  className = ""
+}: {
+  children: React.ReactNode
+  className: string
+}) => {
+  return (
+    <div
+      className={`w-full h-full inline-block z-0 bg-light p-32 ${className}`}
+    >
+      {children}
+    </div>
+  )
+}
