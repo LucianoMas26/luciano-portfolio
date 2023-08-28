9620b5ca43b6a045e04d0d747acb8bd64ca05605
Luciano <lucianoalejandromas@gmail.com>
2023-08-28 18:56:55 -0300

    Mensaje de confirmación

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
-"use client"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Montserrat } from "next/font/google"
 import { Navbar } from "@/components/Navbar/Navbar"
 import Head from "next/head"
 import { Footer } from "@/components/Footer/Footer"
-import { AnimatePresence } from "framer-motion"
-import { usePathname } from "next/navigation"
+
 const montserrat = Montserrat({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -18,7 +16,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const path = usePathname()
   return (
     <html lang="en">
       <Head>
@@ -27,10 +24,7 @@ export default function RootLayout({
       </Head>
       <body className={`${montserrat.className} bg-light w-full min-h-screen`}>
         <Navbar />
-        <AnimatePresence mode="wait">
-          {path}
-          {children}
-        </AnimatePresence>
+        {children}
         <Footer />
       </body>
     </html>
