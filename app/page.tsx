5e8adc0a4cc2ebd2508808b797d302018e3bc490
Luciano <lucianoalejandromas@gmail.com>
2023-08-28 23:05:07 -0300

    Mensaje de confirmación

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/CV LucianoActualizado.pdf"
+                  href="/CV.pdf"
                   target={"_blank"}
                   rel="noopener noreferrer"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark md:p-2 md:px-4 md:text-base"
